3b7e32d1500827ed1cb539d17499e27c88224446
Michael Harris <mwharris7@gmail.com>
2020-06-30 09:31:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,12 +10,10 @@ class Dog
 
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE dogs
-      (
+      CREATE TABLE dogs(
       id INTEGER PRIMARY KEY,
       name TEXT,
-      breed TEXT
-      );
+      breed TEXT);
       SQL
     DB[:conn].execute(sql)
   end
